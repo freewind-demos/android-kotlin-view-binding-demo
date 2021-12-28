2f13198e0bd8f6a00c157c4958f2ab6d95098af7
Freewind <nowindlee@gmail.com>
2021-12-28 23:13:16 +0800

    Convert to build.gradle.kts and update to latest versions

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 31
+    defaultConfig {
+        applicationId = "demos.${rootProject.name.replace('-', '_')}"
+        minSdk = 15
+        targetSdk = 28
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("com.google.dagger:dagger:2.40.5")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("com.android.support.constraint:constraint-layout:2.0.4")
+    annotationProcessor("com.google.dagger:dagger-compiler:2.40.5")
+}
