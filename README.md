1b1a453b792e621ffca964773a5fe50216407ade
Freewind <nowindlee@gmail.com>
2021-12-29 22:14:55 +0800

    android-kotlin-view-binding-demo

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-Android Kotlin Hello World Project
+Android Kotlin View Binding Demo
 ===========================
 
-Run this demo from your IDE.
+使用Android提供的[View Binding](https://developer.android.com/topic/libraries/view-binding)功能，可以代替手动`findViewById`
 
-```
-./gradlew installDebug
-```
+注意：
+1. 需要在`build.gradle`中开启`buildFeatures { viewBinding = true }`
+2. Android会自动为某个Activity生成一个Binding类，可直接引用
+3. 使用`ActivityMainBinding.inflate(layoutInflater)`获取binding
+4. 通过`binding.textField1`等可以引用UI上的某个view，并对其操作
+5. 如果两个EditText之间互相修改内容，需要在更新前做检查以防止无限触发
 
-不能成功部署，需要研究。
+Run `ActivityMain.kt` from your IDE.
+
--- app/src/main/java/demos/MainActivity.kt
+++ app/src/main/java/demos/MainActivity.kt
@@ -1,13 +1,34 @@
 package demos
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import demos.databinding.ActivityMainBinding
+
+fun updateText(editText: EditText): TextWatcher {
+    return object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            // Don't set text to same content to avoid infinite triggering
+            if (editText.text.toString() != s.toString()) {
+                editText.text = s
+            }
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+    }
+}
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textField1.addTextChangedListener(updateText(binding.textField2))
+        binding.textField2.addTextChangedListener(updateText(binding.textField1))
     }
 
 }
